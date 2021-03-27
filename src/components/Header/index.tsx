709efd1467d8f68ee518ae8941aeafef7a4d0c59
myamann <mehmetyaman331@gmail.com>
2021-03-27 16:50:49 +0300

    added routes and header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from 'react-router-dom'
+
+const Tasks: React.FC = () => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar.Brand href="#home">M.YAMAN</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item as={Link} className="nav-link" to="/">Home</Nav.Item>
+          <Nav.Item as={Link} className="nav-link" to="/tasks">Tasks</Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Tasks;
---
+++ src/pages/Tasks/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Tasks: React.FC=() => {
+    return <h1>Tasks page</h1>
+}
+
+export default Tasks;
